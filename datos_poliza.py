3b1a3f59b880f0a16fa76fe413c6cf4916ae7b15
Jesus-Caballero-Cartuistes <caballeroj@utb.edu.co>
2024-04-08 15:58:47 -0500

    first commit

---
+++ datos_poliza.py
@@ -0,0 +1,40 @@
+from abc import ABC
+from pydantic import BaseModel
+
+
+class DatosPoliza(BaseModel, ABC):
+    """
+    Clase abstracta para los datos de una póliza.
+    """
+    tipo_identificacion: str
+    identificacion: int
+    nombre: str
+    apellido: str
+
+
+class PolizaCarro(DatosPoliza):
+    """
+    Clase concreta para una póliza de seguro de carro.
+    """
+    marca: str
+    referencia: str
+    ano: int
+    estado: str
+    uso: str
+
+
+class PolizaMoto(DatosPoliza):
+    """
+    Clase concreta para una póliza de seguro de moto.
+    """
+    cilindraje: float
+
+
+class PolizaVida(DatosPoliza):
+    """
+    Clase concreta para una póliza de seguro de vida.
+    """
+    edad: int
+    beneficios: dict
+    
+    
